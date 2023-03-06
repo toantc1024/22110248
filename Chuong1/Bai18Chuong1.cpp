e49342039816703728a3c563e5ae2998d34cc303
Toan <thanhnoobs@gmail.com>
2023-03-06 15:52:32 +0700

    C1

--- Chuong1/Bai18Chuong1.cpp
+++ Chuong1/Bai18Chuong1.cpp
@@ -1,49 +1,23 @@
-#include <cstdlib>
-#include <iostream>
-#include <ctime>
-#include <iomanip>
+#include <bits/stdc++.h>
+#define N 10000
+#define MIN 1
+#define MAX 32767
+using namespace std;
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::setw;
-
-int getRandom ( int Min, int Max );
-
-char ch;
-
-int userMax = 0;
-
-int main()
-{
-    srand( time ( NULL ) );
-
-    int lotteryArray[ 6 ];
-
-    cout << "Greetings user! What is the maximum possible number you would like for your lottery range? ";
-    cin >> userMax;
-
-    do
-    {
-        for ( int i = 0; i < 6; i++ )
-        {
-            lotteryArray[ i ] = getRandom( 1, 6 );
-        }
-        cout << "Here are your lottery numbers from 1 to " << userMax << "." << endl << endl;
-        for ( int index = 0; index < 6; index++ )
-        {
-            cout << lotteryArray[ index ] << setw( 5 );
-        }
-        cout << endl << "Do you want to play again? Press 'Y' for Yes or any other letter to exit. ";
-        cin >>  ch;
-    }
-    while( ch == 'Y' || ch == 'y' );
-
-    return EXIT_SUCCESS;
+void generateFile() {
+	ofstream fp;
+	fp.open("SONGUYEN.INP");
+	fp.clear();
+	vector<int> a;
+	for(int i = MIN; i < MAX; i++) 
+		a.push_back(i);
+	random_shuffle(a.begin(), a.end());
+	for(int i = 0; i < N; i++) {
+		fp<<a[i]<<" ";
+	}
+	fp.close();
 }
 
-int getRandom ( int Min, int Max )
-{
-    int number = rand() % userMax + 1;
-    return number;
+int main() {
+	generateFile();
 }
